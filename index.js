1bb1be7ac84282499ab781b9a3a3699ebf62f342
TheDeVerO <deverodevero80@gmail.com>
2022-08-07 19:38:44 +0300

    added proper callbacks

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const commands = {};
 
 files.forEach((file) => {
 	if (typeof file === 'object' && file !== null) {
-		console.log('called');
-		commands[file.command] = function callback() {
-			const rand = Math.floor(Math.random() * file.files.length);
-			console.log(rand);
-			// playAudio(file.files[rand]);
+		// console.log('called');
+		commands[file.command] = {
+			callback: function callback() {
+				const rand = Math.floor(Math.random() * file.files.length);
+				console.log(rand);
+				// playAudio(file.files[rand]);
+			},
 		};
 	} else {
-		console.log(file);
 		const split = file.replace(/\\/g, '/').split('/');
+		commands[split[split.length - 1]] = {
+			callback: function callback() {
+				console.log(file);
+			},
+		};
 	}
 });
 
 console.log(commands);
+commands.test.callback();

a45d5330be59a78d452a6a5e7724e80ea1a3f0c7
TheDeVerO <deverodevero80@gmail.com>
2022-08-07 18:08:23 +0300

    changed commands to object, will get a random sound on folder command

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const getFiles = require('./get-files');
 
 const files = getFiles();
-const commands = [];
+const commands = {};
 
 files.forEach((file) => {
-    const split = file.replace(/\\/g, '/').split('/');
-})
+	if (typeof file === 'object' && file !== null) {
+		console.log('called');
+		commands[file.command] = function callback() {
+			const rand = Math.floor(Math.random() * file.files.length);
+			console.log(rand);
+			// playAudio(file.files[rand]);
+		};
+	} else {
+		console.log(file);
+		const split = file.replace(/\\/g, '/').split('/');
+	}
+});
+
+console.log(commands);

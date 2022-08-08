13b2653c163c63db92904408e4d3cebc1ef2cf59
TheDeVerO <deverodevero80@gmail.com>
2022-08-08 08:20:14 +0300

    slightly reworked get-files

--- get-files.js
+++ get-files.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
-function getFiles(dir = `${__dirname}/resources`) {
-	const files = (fs.Dirent = fs.readdirSync(dir, { withFileTypes: true }));
+function getFiles(dir = '') {
+	const files = (fs.Dirent = fs.readdirSync(`${__dirname}/resources/${dir && dir + '/'}`, { withFileTypes: true }));
 
 	const sounds = [];
 
 	files.forEach((file) => {
 		if (file.isDirectory()) {
-			sounds.push({ command: file.name, files: getFiles(`${dir}/${file.name}`) });
+			sounds.push({ command: file.name, files: getFiles(file.name) });
 		} else {
-			sounds.push(`${dir}/${file.name}`);
+			sounds.push(file.name);
 		}
 	});
 

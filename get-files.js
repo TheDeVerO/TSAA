dee7291109023ca420c7c5949784ee4307fa0411
TheDeVerO <deverodevero80@gmail.com>
2022-08-08 20:11:56 +0300

    added an informative message in case there's no files

--- get-files.js
+++ get-files.js
@@ -5,6 +5,11 @@ function getFiles(dir = '') {
 
 	const sounds = [];
 
+	if (files.length === 0) {
+		console.log('No sounds found! Did you forget to add sounds in the "resources" folder?');
+		return [];
+	}
+
 	files.forEach((file) => {
 		if (file.isDirectory()) {
 			sounds.push({ command: file.name, files: getFiles(file.name) });
